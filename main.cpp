2e5597ea0a8e5b5f241efec8a117b2188d3272a0
Jonas Janušauskas <s063449@ad.viko.lt>
2026-09-17 10:00:40 +0300

    strukturinisprogramavimas

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+using namespace  std;
+int main() {
+        string pavadinimas = "Madrido Real";
+        string savininkas = "Klubo nariai";
+        string arena = "Santiago Bernabeu";
+        int ikurimoMetai = 1902;
+        int vietuskaicius:83186
+
+
+        cout << "Sporto klubas: " << pavadinimas << endl;
+        cout << "Ikurimo metai: " << ikurimoMetai << endl;
+        cout << "Savininkas: " << savininkas << endl;
+        cout << "Arena: " << arena << endl;
+        cout << "Vietu skaicius: " << vietuSkaicius << endl;
+
+} // str
